4f405f5d60b4c34b607c2327033649788d37a92c
Okan Erdem <nayko62@gmail.com>
2023-02-01 02:42:54 +0300

    text selection on buttons and more removed, only items

--- components/main.tsx
+++ components/main.tsx
@@ -6,7 +6,7 @@ const Main = () => {
   return (
     <main className={styles.main}>
       <section className={styles.mainBox}>
-        <div className="bg-green-200 p-1 text-center rounded text-green-700">
+        <div className="bg-green-200 p-1 text-center rounded text-green-700 select-none">
           Your item added to the list!
         </div>
         {/* <div className="bg-red-200 p-1 text-center rounded text-red-700">
@@ -14,7 +14,7 @@ const Main = () => {
         </div> */}
         <h2 className="text-2xl font-bold flex items-center justify-center">
           <MdOutlineLocalGroceryStore className="text-4xl" />
-          <sup className="text-xs rounded-full bg-red-500 px-1.5 -ml-4 mb-3 text-white py-[1px]">
+          <sup className="text-xs rounded-full bg-red-500 px-1.5 -ml-4 mb-3 text-white py-[1px] select-none">
             1
           </sup>
         </h2>
@@ -26,12 +26,12 @@ const Main = () => {
         >
           <input
             type="text"
-            className="bg-blue-100 p-2 rounded w-full focus:outline-none"
+            className="bg-blue-100 p-2 rounded w-full focus:outline-none placeholder:select-none"
             placeholder="e.g. yogurt"
           />
           <input
             type="number"
-            className="w-fit rounded bg-white shadow border border-zinc-300 p-2 focus:outline-none text-center focus:placeholder:opacity-0"
+            className="w-fit rounded bg-white shadow border border-zinc-300 p-2 focus:outline-none text-center focus:placeholder:opacity-0 placeholder:select-none"
             min={1}
             max={100}
             placeholder="1"
@@ -39,7 +39,7 @@ const Main = () => {
           />
           <button
             type="submit"
-            className="bg-blue-600 text-blue-100 p-2 rounded w-20 hover:opacity-80 duration-150"
+            className="bg-blue-600 text-blue-100 p-2 rounded w-20 hover:opacity-80 duration-150 select-none"
           >
             Submit
           </button>
@@ -57,7 +57,7 @@ const Main = () => {
           </div>
         </div>
         <div className="flex items-end h-full">
-          <button className="mt-8 bg-red-600 text-red-100 max-w-xs mx-auto w-36 py-2 rounded-md focus:outline-none hover:opacity-80 duration-150">
+          <button className="mt-8 bg-red-600 text-red-100 max-w-xs mx-auto w-36 py-2 rounded-md focus:outline-none hover:opacity-80 duration-150 select-none">
             Reset
           </button>
         </div>
